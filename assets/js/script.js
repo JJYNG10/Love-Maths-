c6dd0649832cae5f29c37785ea249d1bcacb64f6
JJYNG10 <junjie2013@outlook.com>
2026-02-03 21:41:56 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function runGame(){}
+
+function checkAnswer(){}
+
+function calculateCorrectAnswer(){}
+
+function incrementScore(){}
+
+function incrementWongAnswer(){}
+
+function displayAdditionQuestion(){}
+
+function displaySubtractQuestion(){}
+
+function displaymultiplyQuestion(){}
+
