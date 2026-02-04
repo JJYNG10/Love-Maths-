5541843e0889ffb5248528169844489f91afabf8
JJYNG10 <junjie2013@outlook.com>
2026-02-04 22:31:27 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,19 +5,28 @@ for (let button of buttons){
         if (this.getAttribute("data-type")=== "submit")
             {alart("you've clicked submit!");} 
         else{ let gameType = this.getAttribute("data-type"); 
-            alert(`you've clicked ${gameType}`)}
-    })
+            runGame (gameType);}
+    });
 }
+runGame("addition");
 }
-)
+);
 /**
  * the main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
+function runGame(gameType){
 let num1 = Math.floor(Math.random()*25)+1
 let num2 = Math.floor(Math.random()*25)+1
+
+if(gameType==='addition'){
+    displayAdditionQuestion(num1,num2);
+}else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}.Aborting`;
+    }
 }
+
 
 function checkAnswer(){
 
@@ -35,8 +44,10 @@ function incrementWongAnswer(){
 
 }
 
-function displayAdditionQuestion(){
-
+function displayAdditionQuestion(operand1,operand2){
+document.getElementById('operand1').textContent=operand1;
+document.getElementById('operand2').textContent=operand2;
+document.getElementById('operator').textContent="+";
 }
 
 function displaySubtractQuestion(){
