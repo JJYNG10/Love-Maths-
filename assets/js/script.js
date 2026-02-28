53d69320719c2b07242ad6bf6e1216375640b317
JJYNG10 <junjie2013@outlook.com>
2026-02-28 11:14:57 +0000

    Add division

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,9 @@ document.getElementById("answer-box").focus();
         displayMultiplyQuestion(num1,num2);
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-    }
-    else {
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1,num2);
+    } else {
             alert(`unknown game type: ${gameType}`);
             throw `unknown game type: ${gameType}.Aborting`;
         }
@@ -74,6 +75,8 @@ function calculateCorrectAnswer(){
         return [operand1 * operand2, 'multiply'];
     }else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    }else if (operator === '/'){
+        return [operand1 / operand2, "division"];
     }else {
         alert (`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}, aborting... `;}
@@ -108,5 +111,11 @@ function displayMultiplyQuestion(operand1,operand2){
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+}
+
+function displayDivisionQuestion(operand1,operand2){
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
 }
 
